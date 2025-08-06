8cf2cf98832dd1253bbef2ddffaab535cc9821a6
kwarsanaut <109350649+kwarsanaut@users.noreply.github.com>
2025-08-06 11:58:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from typing import Dict, List
 
 # Configure page
 st.set_page_config(
-    page_title="MLOps Platform",
+    page_title="Intelligent Risk Platform",
     page_icon="🤖",
     layout="wide",
     initial_sidebar_state="expanded"
